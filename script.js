47f55097f1e44976eb9ded3bf7f8f365d2110f21
kidsdream <docomo905dream@yahoo.co.jp>
2024-01-24 18:48:56 +0900

    きらさん読み上げ不要対応

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var element_timer = document.querySelector('#timer p span')
 const date1 = new Date();
 const date2 = (date1.getMonth() + 1) + "月" + date1.getDate() + "日" + date1.getHours() + "時" + date1.getMinutes() + "分"
 
-let userIdArray = [];
+let userIdArray = ['42471cc839'];
 
 let isStarted = false
 const uttr = new SpeechSynthesisUtterance()
